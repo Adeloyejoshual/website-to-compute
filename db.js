1f87f9ee194faae4e7df62357b1117c8b6f9b38c
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-11 00:24:45 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,27 +1,14 @@
-
 import pkg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.COCKROACH_URI,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
 });
 
-// Test DB connection on startup
-(async () => {
-  try {
-    const res = await pool.query("SELECT NOW()");
-    console.log("CockroachDB connected:", res.rows[0]);
-  } catch (err) {
-    console.error("CockroachDB connection error:", err);
-  }
-})();
-
-// Add product
 export async function addProduct(name, description, price) {
   const query = `
     INSERT INTO products (name, description, price)
@@ -33,7 +20,6 @@ export async function addProduct(name, description, price) {
   return res.rows[0];
 }
 
-// Get all products
 export async function getProducts() {
   const res = await pool.query("SELECT * FROM products ORDER BY created_at DESC");
   return res.rows;

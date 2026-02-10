8e0a9088355f6a91a8ef81fd50e4591db395bd52
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-02-11 00:32:57 +0100

    Update db.js

--- db.js
+++ db.js
@@ -6,21 +6,18 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.COCKROACH_URI,
-  ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false }
 });
 
 export async function addProduct(name, description, price) {
-  const query = `
-    INSERT INTO products (name, description, price)
-    VALUES ($1, $2, $3)
-    RETURNING *;
-  `;
-  const values = [name, description, price];
-  const res = await pool.query(query, values);
+  const res = await pool.query(
+    `INSERT INTO products (name, description, price) VALUES ($1, $2, $3) RETURNING *`,
+    [name, description, price]
+  );
   return res.rows[0];
 }
 
 export async function getProducts() {
-  const res = await pool.query("SELECT * FROM products ORDER BY created_at DESC");
+  const res = await pool.query(`SELECT * FROM products ORDER BY created_at DESC`);
   return res.rows;
 }

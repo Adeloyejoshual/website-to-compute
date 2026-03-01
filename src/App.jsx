d8350120fb1e8a5050a460c742630d1375d03575
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-01 23:05:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import ProductDetails from "./pages/ProductDetails.jsx";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+import Home from "./pages/Home";
+import AddProduct from "./pages/AddProduct";
+
+export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-      </Routes>
+      <div>
+        {/* Simple Navbar */}
+        <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+          <Link to="/" style={{ marginRight: "15px" }}>Home</Link>
+          <Link to="/add-product">Add Product</Link>
+        </nav>
+
+        {/* Page Content */}
+        <div style={{ padding: "20px" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add-product" element={<AddProduct />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
-
-export default App;

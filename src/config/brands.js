90bf94efb63f80c6a3cd69ff077456815a2de938
Adeloyejoshual <160188634+Adeloyejoshual@users.noreply.github.com>
2026-03-01 22:50:11 +0100

    Create brands.js

---
+++ src/config/brands.js
@@ -0,0 +1,81 @@
+// src/config/brands.js
+
+/**
+ * Brand options for each category
+ * These can be used to populate dropdowns when adding products
+ */
+
+export const brands = {
+  "Phones & Tablets": [
+    "Apple", "Samsung", "Xiaomi", "Infinix", "Tecno", "Huawei", "Google", "OnePlus", "Nokia"
+  ],
+  "Computers & Laptops": [
+    "Apple", "Dell", "HP", "Lenovo", "Asus", "Acer", "MSI", "Microsoft"
+  ],
+  "Gaming Consoles & Games": [
+    "Sony", "Microsoft", "Nintendo", "Sega", "Atari"
+  ],
+  "Electronics & Accessories": [
+    "Sony", "Samsung", "Anker", "Logitech", "Philips", "JBL", "Bose", "Xiaomi"
+  ],
+  "Cameras & Photography": [
+    "Canon", "Nikon", "Sony", "Fujifilm", "GoPro", "Panasonic", "Olympus"
+  ],
+  "Audio & Headphones": [
+    "Sony", "Bose", "JBL", "Sennheiser", "Beats", "Apple", "Audio-Technica"
+  ],
+  "Cars & Trucks": [
+    "Toyota", "Honda", "Ford", "Mercedes-Benz", "BMW", "Hyundai", "Nissan", "Volkswagen"
+  ],
+  "Motorcycles & Scooters": [
+    "Yamaha", "Honda", "Suzuki", "KTM", "Bajaj", "TVS", "Harley-Davidson"
+  ],
+  "Vehicle Parts & Accessories": [
+    "Bosch", "Denso", "Valeo", "Hella", "NGK", "Brembo"
+  ],
+  "Boats & Watercraft": [
+    "Yamaha", "Mercury", "Suzuki", "Honda", "Bayliner"
+  ],
+  "Home & Furniture": [
+    "Ikea", "Ashley", "Wayfair", "Homecrest", "La-Z-Boy", "Sauder"
+  ],
+  "Appliances": [
+    "Samsung", "LG", "Whirlpool", "Bosch", "Haier", "Panasonic"
+  ],
+  "Property - For Rent": [
+    "Generic"
+  ],
+  "Property - For Sale": [
+    "Generic"
+  ],
+  "Fashion - Men": [
+    "Nike", "Adidas", "Puma", "Levi's", "Zara", "H&M"
+  ],
+  "Fashion - Women": [
+    "Gucci", "Prada", "Zara", "H&M", "Nike", "Adidas"
+  ],
+  "Pets - Live Animals": [
+    "Generic"
+  ],
+  "Pet Supplies": [
+    "Pedigree", "Whiskas", "Royal Canin", "Hill's", "Purina"
+  ],
+  "Books & Educational Materials": [
+    "Penguin", "Oxford", "Cambridge", "Random House"
+  ],
+  "Musical Instruments": [
+    "Yamaha", "Fender", "Gibson", "Roland", "Casio"
+  ],
+  "Sports Equipment": [
+    "Nike", "Adidas", "Puma", "Wilson", "Spalding"
+  ],
+  "Art & Antiques": [
+    "Generic"
+  ],
+  "Collectibles & Memorabilia": [
+    "Generic"
+  ],
+  "Services": [
+    "Generic"
+  ]
+};
